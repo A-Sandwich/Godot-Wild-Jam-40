5dfc3b50adf9c75e2239a3de02dec330d51f8172
Kyle <kbburkholder@gmail.com>
2021-12-16 17:23:11 -0500

    Fixed ray casting bug

--- Cozy/Player.gd
+++ Cozy/Player.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
 var is_moving = false
-var starting_position = Vector2.ZERO
-var destination_position = Vector2.ZERO
+var starting_position = Vector2(8, 8)
+var destination_position = Vector2(24, 8)
+var offset = Vector2(8, 8)
 var grid_size = 16
 var movement_speed = 100
 var movement_direction = Vector2.ZERO
 
 
-
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _physics_process(delta):
@@ -19,20 +19,28 @@ func _physics_process(delta):
 	move(delta)
 
 func move(delta):
-	if global_position.distance_to(destination_position) < 1:
-		global_position = destination_position
+	if (global_position + offset).distance_to(destination_position) < 1:
+		global_position = destination_position - offset
 		is_moving = false
 		return
-	print(global_position)
+
 	var collision = move_and_collide(movement_direction * movement_speed * delta)
 	if collision:
-		print("collission")
 		movement_direction *= -1
 		destination_position = starting_position
 	
 
 func process_input():
 	if is_moving:
+		return
+
+	if Input.is_action_just_pressed("action_button"):
+		$AnimatedSprite.play("axe")
+		return
+	if Input.is_action_just_released("action_button"):
+		$AnimatedSprite.stop()
+	
+	if $AnimatedSprite.is_playing() and $AnimatedSprite.animation == "axe":
 		return
 
 	movement_direction = Vector2.ZERO
@@ -45,24 +53,49 @@ func process_input():
 	elif Input.is_action_pressed("move_down"):
 		movement_direction.y = 1
 	if movement_direction != Vector2.ZERO:
-		is_moving = true
-		starting_position = global_position
-		destination_position = global_position + (movement_direction * grid_size)
-		print(global_position, destination_position)
+		var original_position = global_position
+		starting_position = global_position + offset
+		destination_position = starting_position + (movement_direction * grid_size)
+		print("start", starting_position, "dest", destination_position, movement_direction * grid_size)
+		update()
+		get_parent().get_node("circle").global_position = destination_position
+		if is_position_invalid():
+			destination_position = original_position
+			is_moving = false
+			movement_direction = Vector2.ZERO
+			print("Position invalid")
+			return
+		else:
+			is_moving = true
+
+func _draw():
+	print("drawing", starting_position, destination_position)
+	draw_line(to_local(starting_position), to_local(destination_position), Color(1, 1, 0))
+
+func is_position_invalid():
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	print(starting_position, destination_position, "is_invalid?")
+	var result = space_state.intersect_ray(starting_position, destination_position, [self, $CollisionShape2D], 1)
+	if result.empty():
+		print("valid")
+	else:
+		print("invalid")
+	return !result.empty()
 
 func choose_animation():
-	if $AnimatedSprite.playing and not is_moving:
+	if $AnimatedSprite.playing and not is_moving and $AnimatedSprite.animation == "walk":
 		$AnimatedSprite.stop()
 	
 	if is_moving:
 		if movement_direction.x == 1:
 			$AnimatedSprite.flip_h = false
-			if not $AnimatedSprite.animation != "walk":
+			if not $AnimatedSprite.animation != "walk" or not $AnimatedSprite.is_playing():
 				$AnimatedSprite.play("walk")
 		elif movement_direction.x == -1:
 			$AnimatedSprite.flip_h = true
-			if not $AnimatedSprite.animation != "walk":
+			if not $AnimatedSprite.animation != "walk" or not $AnimatedSprite.is_playing():
 				$AnimatedSprite.play("walk")
-		elif not $AnimatedSprite.animation != "walk":
+		elif not $AnimatedSprite.animation != "walk" or not $AnimatedSprite.is_playing():
 			$AnimatedSprite.play("walk")
 		
---
+++ Cozy/TargetChecker.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func is_position_invalid(direction):
+	if direction.x == 1:
+		return right.is
