8797c3fe18672ad7dce79db7e6ed1fac25b93b58
Kyle <kbburkholder@gmail.com>
2021-12-16 19:33:54 -0500

    chop chop

--- Cozy/Player.gd
+++ Cozy/Player.gd
@@ -7,11 +7,11 @@ var offset = Vector2(8, 8)
 var grid_size = 16
 var movement_speed = 100
 var movement_direction = Vector2.ZERO
+var last_direction_moved = Vector2.ZERO
 
 
 func _ready():
 	pass
-
 
 func _physics_process(delta):
 	process_input()
@@ -36,9 +36,11 @@ func process_input():
 
 	if Input.is_action_just_pressed("action_button"):
 		$AnimatedSprite.play("axe")
+		chop_tree()
 		return
 	if Input.is_action_just_released("action_button"):
 		$AnimatedSprite.stop()
+		$AnimatedSprite.animation = "walk"
 	
 	if $AnimatedSprite.is_playing() and $AnimatedSprite.animation == "axe":
 		return
@@ -53,12 +55,8 @@ func process_input():
 	elif Input.is_action_pressed("move_down"):
 		movement_direction.y = 1
 	if movement_direction != Vector2.ZERO:
-		var original_position = global_position
-		starting_position = global_position + offset
-		destination_position = starting_position + (movement_direction * grid_size)
-		print("start", starting_position, "dest", destination_position, movement_direction * grid_size)
+		var original_position = populate_target_destination()
 		update()
-		get_parent().get_node("circle").global_position = destination_position
 		if is_position_invalid():
 			destination_position = original_position
 			is_moving = false
@@ -67,21 +65,33 @@ func process_input():
 			return
 		else:
 			is_moving = true
+			last_direction_moved = movement_direction
 
 func _draw():
 	print("drawing", starting_position, destination_position)
 	draw_line(to_local(starting_position), to_local(destination_position), Color(1, 1, 0))
 
 func is_position_invalid():
-	var space_state = get_world_2d().direct_space_state
-	# use global coordinates, not local to node
-	print(starting_position, destination_position, "is_invalid?")
-	var result = space_state.intersect_ray(starting_position, destination_position, [self, $CollisionShape2D], 1)
+	var result = check_destination()
 	if result.empty():
 		print("valid")
 	else:
 		print("invalid")
 	return !result.empty()
+
+func populate_target_destination():
+	var original_position = global_position
+	var move_vector = movement_direction if movement_direction != Vector2.ZERO else last_direction_moved
+	starting_position = global_position + offset
+	destination_position = starting_position + (move_vector * grid_size)
+	print("start", starting_position, "dest", destination_position, move_vector * grid_size)
+	return original_position
+	
+func check_destination():
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var result = space_state.intersect_ray(starting_position, destination_position, [self, $CollisionShape2D], 1)
+	return result
 
 func choose_animation():
 	if $AnimatedSprite.playing and not is_moving and $AnimatedSprite.animation == "walk":
@@ -98,4 +108,13 @@ func choose_animation():
 				$AnimatedSprite.play("walk")
 		elif not $AnimatedSprite.animation != "walk" or not $AnimatedSprite.is_playing():
 			$AnimatedSprite.play("walk")
-		
+
+func chop_tree():
+	populate_target_destination()
+	var target = check_destination()
+	if target.empty():
+		return
+	var collider = target.collider
+	print(collider.get_parent().name)
+	if "Tree" in collider.get_parent().name:
+		print("Chop chop")
